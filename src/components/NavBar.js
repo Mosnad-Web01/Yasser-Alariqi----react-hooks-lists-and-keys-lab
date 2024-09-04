cf6cbe017565d234a8550543821eeca6a565f3ca
YasserEsam <yaaser10esam1010@gmail.com>
2024-09-04 14:11:58 +0300

    pass the test and make the list

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,17 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return(
+    <nav>
+      {links.map((item, index) => {
+        return (
+          <a href={`#${item}`} key={index}>{item}</a>
+        )
+      })}
+    </nav>
+    )
+  
+  ;
 }
 
 export default NavBar;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,13 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projects.map((pro , index) => {
+          return (
+          <ProjectItem key={index} name={pro.name} about={pro.about} technologies={pro.technologies}/>
+          )
+        })}
+      </div>
     </div>
   );
 }
